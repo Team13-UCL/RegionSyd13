69fb00f9bc485dc7d3e3e1aa7b1501e851e4ee22
Nicolai.Tofteby <nicolaitofteby@gmail.com>
2024-09-20 11:16:13 +0200

    Login database + system virker

--- RegionSyd13/2.View/LoginView.xaml.cs
+++ RegionSyd13/2.View/LoginView.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using RegionSyd13._1.Model;
 using RegionSyd13._3.ViewModel;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace RegionSyd13._2.View
 {
@@ -39,9 +41,39 @@ namespace RegionSyd13._2.View
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            TaskListView taskListView = new TaskListView(taskBankViewModel);
-            taskListView.Show();
-            this.Close();
+            // Database connection
+            using (SqlConnection con = new SqlConnection(@"Data Source=hedhed.database.windows.net;Initial Catalog=Login_RegionSyd13;User ID=Featherlance;Password=Nisse197512"))
+            {
+                con.Open();
+
+                // Prepare query using parameters
+                string query = "SELECT Username, Password FROM UserTable WHERE Username = @username AND Password = @password";
+
+                // Create SQL command with parameters to prevent SQL injection
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@username", UserIdTextBox.Text);
+                    cmd.Parameters.AddWithValue("@password", PasswordBox.Password);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        MessageBox.Show("Login Success");
+                        // Proceed to the next view, for example:
+                        TaskListView taskListView = new TaskListView(taskBankViewModel);
+                        taskListView.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Failed");
+                    }
+                }
+            }
+
         }
     }
 }
---
+++ User/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserID] NCHAR(10) NULL, 
+    [Password] NCHAR(10) NULL
+)
